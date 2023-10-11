c03146f6bc378a43dbdb83bec53950e8b18334ed
victoriabernadskiy <vbernadskiy0531@gmail.com>
2023-10-11 08:44:31 -0400

    start

---
+++ app.js
@@ -0,0 +1,25 @@
+let mushrooms=[Enoki, Portobello, Shiitake, Chanterelle, Morel]
+
+const mushrooms = [
+    {
+        name: "Enoki",
+        color: "white",
+        texture: "crunchy",
+        flavor: "mild",
+        common: "yes"
+    },
+    {
+        name: "Portobello",
+        color: "brown",
+        texture: "dense",
+        flavor: ["rich", "savory"],
+        common: "yes"
+    },
+    {
+        name: "Shiitake",
+        color: "brown",
+        texture: "chewy",
+        flavor: ""
+    }
+
+]
