c6beb9add26a645214a8c14490b0c7fe10e66cd3
victoriabernadskiy <vbernadskiy0531@gmail.com>
2023-10-11 09:24:53 -0400

    somewherrer

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
-let mushrooms=[Enoki, Portobello, Shiitake, Chanterelle, Morel]
 
-const mushrooms = [
-    {
+const Enoki = {
         name: "Enoki",
         color: "white",
         texture: "crunchy",
-        flavor: "mild",
-        common: "yes"
-    },
-    {
+        flavor: ["mild", "sweet","blissful", "satisying"],
+        common: "yes",
+}
+const Portobello = {
         name: "Portobello",
         color: "brown",
         texture: "dense",
-        flavor: ["rich", "savory"],
+        flavor: ["rich", "savory", "woodsy", "strong"],
         common: "yes"
-    },
-    {
+    }
+const Shiitake = {
         name: "Shiitake",
         color: "brown",
         texture: "chewy",
-        flavor: ""
+        flavor: ["earthy", "smoky", "satisfying", "strong"],
+        common: "yes"
+    }
+const Chanterelle = {
+        name: "Chanterelle",
+        color: "yellow",
+        texture: "chewy",
+        flavor: ["smoky", "mild", "woodsy", "savory"],
+        common: "no"
     }
 
-]
+const Morel = {
+    
+        name: "Morel",
+        color: ["brown", "white"],
+        texture: ["slimy", "dense"],
+        flavor: ["earthy", "woodsy", "nutty", "blissful", "strong"],
+        common: "yes"
+    }
+
+let mushrooms=[Enoki, Portobello, Shiitake, Chanterelle, Morel]
+
+mushrooms.forEach((mushrooms) => console.log(mushrooms.name));
+mushrooms.forEach(function(flavor) {
+    console.log(flavor);
+  });
+
+
